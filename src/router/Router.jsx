2f12d5aaf331082f91523581ba41aba360c2ed09
new37-33 <69107086+new37-33@users.noreply.github.com>
2021-09-18 05:51:53 +0900

    Initial commit

---
+++ src/router/Router.jsx
@@ -0,0 +1,59 @@
+import { Switch, Route } from "react-router-dom";
+
+import { Home } from "../Home";
+import { page1Routes } from "./Page1Routes";
+import { page2Routes } from "./Page2Routes";
+
+export const Router = () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route
+        path="/page1"
+        render={({ match: { url } }) => {
+          // console.log(props);
+          return (
+            <Switch>
+              {page1Routes.map((route) => {
+                // console.log(route);
+                return (
+                  <Route
+                    key={route.path}
+                    exact={route.exact}
+                    path={`${url}${route.path}`}
+                  >
+                    {route.children}
+                  </Route>
+                );
+              })}
+            </Switch>
+          );
+        }}
+      ></Route>
+      <Route
+        path="/page2"
+        render={({ match: { url } }) => {
+          // console.log(props);
+          return (
+            <Switch>
+              {page2Routes.map((route) => {
+                // console.log(route);
+                return (
+                  <Route
+                    key={route.path}
+                    exact={route.exact}
+                    path={`${url}${route.path}`}
+                  >
+                    {route.children}
+                  </Route>
+                );
+              })}
+            </Switch>
+          );
+        }}
+      ></Route>
+    </Switch>
+  );
+};
